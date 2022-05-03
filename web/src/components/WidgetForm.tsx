8d2dcd970a3761c5acea1b2a1cdd3403f6ef846c
Daniel Nunes <danielzcomputacao@gmail.com>
2022-05-03 17:07:58 -0300

    first class

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -16,7 +16,7 @@ export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[(calc(100vw-2rem)] md:w-auto">
       <header>
-        <span className="text-xl leading-6">Deixe seu Feedbacks/span>
+        <span className="text-xl leading-6">Deixe seu Feedback</span>
 
         <CloseButton />
       </header>
